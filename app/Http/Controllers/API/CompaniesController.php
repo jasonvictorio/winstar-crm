fc70c123657e2d584c002157dbeec0b51a012cbf
Jason Victorio <jgvictorio@gmail.com>
2019-08-21 15:16:57 +1200

    api: create companies controller

---
+++ app/Http/Controllers/API/CompaniesController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace WinstarCRM\Http\Controllers\API;
+
+use WinstarCRM\Http\Controllers\API\BaseController;
+
+class CompaniesController extends BaseController
+{
+    function __construct() {
+        parent::__construct('companies');
+    }
+}
