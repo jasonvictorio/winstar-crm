513a41d41caadda3741ca704ee717f6d3719d48f
Jason Victorio <jgvictorio@gmail.com>
2019-09-11 22:46:01 +1200

    convert companies module

--- resources/views/companies.blade.php
+++ resources/views/companies.blade.php
@@ -3,38 +3,12 @@
 @section('title', 'Companies')
 
 @section('content')
-<div class="card-body">
-    @if (session('status'))
-    <div class="alert alert-success" role="alert">
-        {{ session('status') }}
-    </div>
-    @endif
-    <div class="col-md-12">
-        <?php
-            use WinstarCRM\Company;
-            use WinstarCRM\Helpers\CRMGrid;
-
-            $newgrid = new CRMGrid(Company::class, 'Companies Table',
-                array(
-                'column_configs' => array(
-                    'id' => array('header'=>'ID', 'type' => 'text', 'length' => 5),
-                    'name' => array('header'=>'Name', 'length' => 20),
-                    'address' => array('header'=>'Address', 'length' => 50)
-                ),
-                'table_config' => array(
-                    'enable_add' => true,
-                    'enable_edit' => true,
-                    'is_editable' => true,
-                    'enable_delete' => true,
-                    'is_sortable' => true,
-                    'set_size' => '-sm',
-                    'paginate' => 10,
-                    'enable_filter' => true,
-                    'constraint' => array('id', '>', 1)
-                ))
-            );
-            $newgrid->display();
-        ?>
-    </div>
-</div>
+  <table-component
+    api-endpoint="companies"
+    display-property="name"
+    :columns="[
+      { property: 'name', label: 'Name' },
+      { property: 'address', label: 'Address' },
+    ]"
+  />
 @endsection
