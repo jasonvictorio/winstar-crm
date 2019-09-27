a594c1ce99fb01ea70b35f11e21ed9b1447f44f9
Jason Victorio <jgvictorio@gmail.com>
2019-09-27 22:06:32 +1200

    update seeder, use factory

--- database/factories/CompanyFactory.php
+++ database/factories/CompanyFactory.php
@@ -1,10 +1,151 @@
 <?php
 
 use Faker\Generator as Faker;
+use Carbon\Carbon;
 
 $factory->define(WinstarCRM\Company::class, function (Faker $faker) {
     return [
         'name' => $faker->company,
         'address' => $faker->address,
+        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+    ];
+});
+
+$factory->define(WinstarCRM\User::class, function (Faker $faker) {
+    $company_ids = WinstarCRM\Company::all()->pluck('id')->toArray();
+    $role_ids = WinstarCRM\Role::all()->pluck('id')->toArray();
+    return [
+        'company_id' => $faker->randomElement($company_ids),
+        'role_id' => $faker->randomElement($role_ids),
+        'name' => $faker->name,
+        'avatar' => 'default.png',
+        'email' => $faker->unique()->safeEmail,
+        'email_verified_at' => now(),
+        'password' => bcrypt('password'),
+        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+    ];
+});
+
+$factory->define(WinstarCRM\StatusType::class, function (Faker $faker) {
+    $company_ids = WinstarCRM\Company::all()->pluck('id')->toArray();
+    $user_ids = WinstarCRM\User::all()->pluck('id')->toArray();
+    return [
+        'name' => $faker->numerify('Status type ##'),
+        'company_id' => $faker->randomElement($company_ids),
+        'user_id' => $faker->randomElement($user_ids),
+        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+    ];
+});
+
+$factory->define(WinstarCRM\Status::class, function (Faker $faker) {
+    $company_ids = WinstarCRM\Company::all()->pluck('id')->toArray();
+    $user_ids = WinstarCRM\User::all()->pluck('id')->toArray();
+    $status_type_ids = WinstarCRM\StatusType::all()->pluck('id')->toArray();
+    return [
+        'name' => $faker->numerify('Status ##'),
+        'company_id' => $faker->randomElement($company_ids),
+        'user_id' => $faker->randomElement($status_type_ids),
+        'status_type_id' => $faker->randomElement($status_type_ids),
+        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+    ];
+});
+
+$factory->define(WinstarCRM\NatureOfContact::class, function (Faker $faker) {
+    $company_ids = WinstarCRM\Company::all()->pluck('id')->toArray();
+    $user_ids = WinstarCRM\User::all()->pluck('id')->toArray();
+    return [
+        'name' => $faker->numerify('Nature of contact ##'),
+        'company_id' => $faker->randomElement($company_ids),
+        'user_id' => $faker->randomElement($user_ids),
+        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+    ];
+});
+
+$factory->define(WinstarCRM\Customer::class, function (Faker $faker) {
+    $company_ids = WinstarCRM\Company::all()->pluck('id')->toArray();
+    $user_ids = WinstarCRM\User::all()->pluck('id')->toArray();
+    $status_ids = WinstarCRM\Status::all()->pluck('id')->toArray();
+    $nature_of_contact_ids = WinstarCRM\NatureOfContact::all()->pluck('id')->toArray();
+    return [
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
+        'company_id' => $faker->randomElement($company_ids),
+        'user_id' => $faker->randomElement($user_ids),
+        'status_id' => $faker->randomElement($status_ids),
+        'nature_of_contact_id' => $faker->randomElement($nature_of_contact_ids),
+        'first_contacted' => $faker->dateTimeBetween('-2 months', 'now'),
+        'last_contacted' => $faker->dateTimeBetween('now', '2 months'),
+        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+    ];
+});
+
+$factory->define(WinstarCRM\Project::class, function (Faker $faker) {
+    $company_ids = WinstarCRM\Company::all()->pluck('id')->toArray();
+    $user_ids = WinstarCRM\User::all()->pluck('id')->toArray();
+    $status_ids = WinstarCRM\Status::all()->pluck('id')->toArray();
+    $customer_ids = WinstarCRM\Customer::all()->pluck('id')->toArray();
+    return [
+        'name' => $faker->numerify('Project ##'),
+        'start_date' => $faker->dateTimeBetween('-2 months', 'now'),
+        'estimated_end_date' => $faker->dateTimeBetween('now', '2 months'),
+        'company_id' => $faker->randomElement($company_ids),
+        'user_id' => $faker->randomElement($user_ids),
+        'status_id' => $faker->randomElement($status_ids),
+        'customer_id' => $faker->randomElement($customer_ids),
+        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+    ];
+});
+
+$factory->define(WinstarCRM\TaskType::class, function (Faker $faker) {
+    $company_ids = WinstarCRM\Company::all()->pluck('id')->toArray();
+    $user_ids = WinstarCRM\User::all()->pluck('id')->toArray();
+    return [
+        'name' => $faker->numerify('Task type ##'),
+        'company_id' => $faker->randomElement($company_ids),
+        'user_id' => $faker->randomElement($user_ids),
+        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+    ];
+});
+
+$factory->define(WinstarCRM\TaskCategory::class, function (Faker $faker) {
+    $company_ids = WinstarCRM\Company::all()->pluck('id')->toArray();
+    $user_ids = WinstarCRM\User::all()->pluck('id')->toArray();
+    return [
+        'name' => $faker->numerify('Task category ##'),
+        'company_id' => $faker->randomElement($company_ids),
+        'user_id' => $faker->randomElement($user_ids),
+        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+    ];
+});
+
+$factory->define(WinstarCRM\Task::class, function (Faker $faker) {
+    $company_ids = WinstarCRM\Company::all()->pluck('id')->toArray();
+    $user_ids = WinstarCRM\User::all()->pluck('id')->toArray();
+    $project_ids = WinstarCRM\Project::all()->pluck('id')->toArray();
+    $status_ids = WinstarCRM\Status::all()->pluck('id')->toArray();
+    $task_type_ids = WinstarCRM\TaskType::all()->pluck('id')->toArray();
+    $task_category_ids = WinstarCRM\TaskCategory::all()->pluck('id')->toArray();
+    return [
+        'name' => $faker->numerify('Task ##'),
+        'description' => $faker->sentence,
+        'created_date' => $faker->dateTimeBetween('-2 months', 'now'),
+        'deadline_date' => $faker->dateTimeBetween('now', '2 months'),
+        'company_id' => $faker->randomElement($company_ids),
+        'user_id' => $faker->randomElement($user_ids),
+        'project_id' => $faker->randomElement($project_ids),
+        'status_id' => $faker->randomElement($status_ids),
+        'task_type_id' => $faker->randomElement($task_type_ids),
+        'task_category_id' => $faker->randomElement($task_category_ids),
+        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -72,82 +72,17 @@ class DatabaseSeeder extends Seeder
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
             'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]]);
+        factory(WinstarCRM\Company::class, 4)->create();
+        factory(WinstarCRM\User::class, 4)->create();
+        factory(WinstarCRM\StatusType::class, 5)->create();
+        factory(WinstarCRM\Status::class, 5)->create();
+        factory(WinstarCRM\NatureOfContact::class, 5)->create();
+        factory(WinstarCRM\Customer::class, 50)->create();
+        factory(WinstarCRM\Project::class, 50)->create();
+        factory(WinstarCRM\TaskType::class, 5)->create();
+        factory(WinstarCRM\TaskCategory::class, 5)->create();
+        factory(WinstarCRM\Task::class, 50)->create();
 
         // \WinstarCRM\User::find(1)->sendEmailVerificationNotification();
-
-        DB::table('status_types')->insert([
-            'name' => 'Status type 1',
-            'company_id' => 1,
-            'user_id' => 1,
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        ]);
-        DB::table('status')->insert([
-            'name' => 'Status 1',
-            'company_id' => 1,
-            'user_id' => 1,
-            'status_type_id' => 1,
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        ]);
-        DB::table('nature_of_contacts')->insert([
-            'name' => 'Nature of contact 1',
-            'company_id' => 1,
-            'user_id' => 1,
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        ]);
-        DB::table('customers')->insert([
-            'first_name' => 'Tony',
-            'last_name' => 'Stark',
-            'company_id' => 1,
-            'user_id' => 1,
-            'status_id' => 1,
-            'nature_of_contact_id' => 1,
-            'hear_about_us' => 'On the internets',
-            'first_contacted' => Carbon::now()->format('Y-m-d H:i:s'),
-            'last_contacted' => Carbon::now()->format('Y-m-d H:i:s'),
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        ]);
-        DB::table('projects')->insert([
-            'name' => 'Project 1',
-            'start_date' => Carbon::now()->format('Y-m-d H:i:s'),
-            'estimated_end_date' => Carbon::now()->format('Y-m-d H:i:s'),
-            'company_id' => 1,
-            'user_id' => 1,
-            'status_id' => 1,
-            'customer_id' => 1,
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        ]);
-        DB::table('task_types')->insert([
-            'name' => 'Task Type 1',
-            'company_id' => 1,
-            'user_id' => 1,
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        ]);
-        DB::table('task_categories')->insert([
-            'name' => 'Task Category 1',
-            'company_id' => 1,
-            'user_id' => 1,
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        ]);
-        DB::table('tasks')->insert([
-            'name' => 'Task 1',
-            'description' => 'this is the task 1',
-            'created_date' => Carbon::now()->format('Y-m-d H:i:s'),
-            'deadline_date' => Carbon::now()->format('Y-m-d H:i:s'),
-            'project_id' => 1,
-            'status_id' => 1,
-            'task_type_id' => 1,
-            'task_category_id' => 1,
-            'company_id' => 1,
-            'user_id' => 1,
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        ]);
     }
 }
